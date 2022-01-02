4ca43be0ee29660067a7c94716bc62f32b22bef1
splakhotni <sergzemlyak33@gmail.com>
2022-01-02 18:48:38 +0200

    Добавив script.js

---
+++ OurPractices/hmstr_01/LoftMebel_boostrap/script.js
@@ -0,0 +1,14 @@
+//const bodyElement = document.body;
+//const  firstChildNode = bodyElement.firstChild;
+//const  lastChildNode = bodyElement.lastChild;
+//console.log(firstChildNode);
+//console.log(lastChildNode);
+//const  childNodes = bodyElement.childNodes;
+//console.log(childNodes);
+//console.log(bodyElement.hasChildNodes());
+
+
+
+
+
+
